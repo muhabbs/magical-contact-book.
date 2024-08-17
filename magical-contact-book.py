8b293baf126177e97b08f890439b9e7a4d0018af
muhabbs <mohababdelgwad260@gmail.com>
2024-08-17 18:57:08 +0300

    Add files via upload
    This is the last and final form for this code

--- magical-contact-book.py
+++ magical-contact-book.py
@@ -4,16 +4,16 @@ class MagicalContact:
         self._email = email
         self._phone_number = phone_number
     def name(self):
-        return self_name
-def email(self):
+        return self._name
+    def email(self):
         return self._email
-def phone_number(self):
+    def phone_number(self):
         return self._phone_number
-     def email(self, email):
+    def email(self, email):
         self._email = email
- def phone_number(self, phone_number)
+    def phone_number(self, phone_number):
         self._phone_number = phone_number
- def describe(self):
+    def describe(self):
         print(f"Name: {self._name}, Email: {self._email}, Phone: {self._phone_number}")
 class Wizard(MagicalContact):
     def __init__(self, name, email, phone_number, wand, house):
@@ -22,9 +22,9 @@ class Wizard(MagicalContact):
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             print("invalid")
         self._house = house
-def wand(self):
+    def wand(self):
         return f"{self._wand['length']} cm , {self._wand['wood']}, {self._wand['core']}"
-      def house(self):
+    def house(self):
         return self._house
 
     def describe(self):
@@ -42,7 +42,7 @@ class MagicalContactBook:
         for contact in self._contacts:
             contact.describe()
             print()
- def find_contact(self, name):
+    def find_contact(self, name):
         for contact in self._contacts:
             if contact.name == name:
                 contact.describe()
