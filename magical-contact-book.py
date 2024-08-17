34762f3b7f7b84d0512af38e03c90d5f08a19664
muhabbs <mohababdelgwad260@gmail.com>
2024-08-17 18:11:31 +0300

    Update magical-contact-book.py

---
+++ magical-contact-book.py
@@ -0,0 +1,9 @@
+class MagicalContact:
+    def __init__(self, name, email, phone_number):
+        self._name = name
+        self._email = email
+        self._phone_number = phone_number
+    def name(self):
+        return self._name
+def email(self):
+        return self._email
